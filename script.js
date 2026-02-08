06865dd07effb55b22f41fb50430d7ad1f85a811
Sharon Moegi <sharamoegi@gmail.com>
2026-02-08 22:59:31 +0300

    Create  'Add Contact method'

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ addressBook.prototype.assignId = function(){
     this.currentId++;
     return this.currentId;
 };
+
+addressBook.prototype.addContact = function(contact) {
+    contact.id = this.assignId();
+    this.contacts[contact.id] = contact;
+    console.log(`[LOG] Contact Added Successfully`);
+};
+
